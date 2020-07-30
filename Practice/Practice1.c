620ee37fb3b287f44e2600b7a9b01fe07bdc1a82
Aman Gwari <amangwari29sep@gmail.com>
2020-07-30 22:51:33 +0530

    changing print

--- Practice/Practice1.c
+++ Practice/Practice1.c
@@ -5,7 +5,7 @@ int main()
 {   
     int a, b, c;
     char d, e;           // if we dont declare e initially so in result it will show 'e'= undecleared
-    printf("Hello world");
+    printf("Hello Aman");
     a = 3;
     b = 5;
     c = a + b;
