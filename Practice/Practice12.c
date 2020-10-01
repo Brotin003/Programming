0555b45efbd1eaf5fe032f1ab6912b154641046e
Brotin003 <72161482+Brotin003@users.noreply.github.com>
2020-10-01 15:22:09 +0530

    Update Practice12.c

--- Practice/Practice12.c
+++ Practice/Practice12.c
@@ -12,7 +12,7 @@ int main()
     }
     else
     {
-        printf("The number is not divisible by 5 and 11");
+        printf("The number is not divisible by 5 and 11 done");
     }
 
     return 0;
